919f9960c7ef98b7c9cc8ea02b449da505e7971b
wechoi <wechoi@163.com>
2018-09-18 11:00:20 +0800

    hadmin init

---
+++ hadmin.api/src/test/java/com/hh/hadmin/HadminapiApplicationTests.java
@@ -0,0 +1,16 @@
+package com.hh.hadmin;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class HadminapiApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
